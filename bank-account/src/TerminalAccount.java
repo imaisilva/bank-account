af66cef7d8ed537f9ac744524f4887171b28f778
Mayara Silva <alohasp@outlook.com>
2025-04-03 14:40:50 -0300

    Terminal account

--- bank-account/src/TerminalAccount.java
+++ bank-account/src/TerminalAccount.java
@@ -1,5 +1,19 @@
 public class TerminalAccount {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        int accountNumber = Integer.parseInt(args[0]);
+        String agency = args[1];
+        String customerName = args[2];
+        String customerLastName =args[3];
+        Double balance = Double.parseDouble(args[4]);
+
+        System.out.println("Enter account number: " + accountNumber);
+        System.out.println("Enter agency: " + agency);
+        System.out.println("Enter name: " + customerName);
+        System.out.println("Enter last name: " + customerLastName);
+        System.out.println("Enter balance: " + balance);
+
+        System.out.println("Hi " + customerName + " " + customerLastName + ", thanks for creating with our bank, your agency is " + agency + ", account " + accountNumber + " and your balance " + balance + " it's now available for withdrawal! ");
     }
 }
